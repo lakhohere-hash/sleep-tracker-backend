794d69c0af58fa53f5d926889887adb905af3930
lakhohere-hash <lakhohere@gmail.com>
2025-11-17 21:23:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.json());
 console.log('🚀 Starting Sleep Tracker Backend...');
 
 // MongoDB Configuration
-const MONGODB_URI = 'mongodb+srv://sleepapp:SleepApp12345@cluster0.qyenjoe.mongodb.net/sleep_tracker?retryWrites=true&w=majority&appName=Cluster0';let client = null;
-
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://sleepapp:SleepApp12345@cluster0.qyenjoe.mongodb.net/sleep_tracker?retryWrites=true&w=majority&appName=Cluster0';
 // Connect to MongoDB
 async function connectDB() {
     try {
